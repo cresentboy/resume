0175d60fc8f0b1af1416f221241b8a23a9421fc5
zhigang <fdrgdf@foxmail.com>
2022-05-10 23:08:43 +0800

    foor

--- src/components/common/foot.vue
+++ src/components/common/foot.vue
@@ -1,8 +1,8 @@
 <template>
   <footer class="github-footer">
-    <a class="footer-link" href="https://github.com/cresentboy/online-resume"
+    <!-- <a class="footer-link" href="https://github.com/cresentboy/online-resume"
       >项目库托管在Github</a
-    >
+    > -->
   </footer>
 </template>
 <script>
